e12e4f5a549e9444c2845807bfd0de89e8ebe92e
sundehakon <hakon.su@gmail.com>
2024-09-12 09:08:15 +0000

    Add CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 mod modules;
-use modules::readjson::{self, Todo};
+use modules::readjson;
+use std::io;
 
 fn main() {
-    let todos = vec![
-        Todo { text: String::from("Finish projects")}
-    ];
+    println!("Welcome to your personal To-Do list!");
+    println!("1. See To-Do list");
+    println!("2. Write new To-Do");
+    // Add logic for removing items from list
+    println!("3. Remove from To-Do list");
 
-    readjson::read_json_file("src/data.json".to_string());
-    readjson::write_json_file("src/data.json".to_string(), &todos);
+    let mut input = String::new();
+    println!("Choose your operation:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let input: i32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a number");
+            return;
+        }
+    };
+
+    if input > 3 {
+        println!("Please choose a number from the list!");
+    }
+
+    if input == 1 {
+        let todos = readjson::read_json_file("src/data.json".to_string());
+        for todo in todos {
+            println!("Todo: {}", todo.text);
+        }
+    }
+
+    if input == 2 {
+        let new_todo = readjson::get_user_input();
+        readjson::write_json_file("src/data.json".to_string(), new_todo);
+    }
 }
--- src/modules/readjson.rs
+++ src/modules/readjson.rs
@@ -2,25 +2,45 @@ use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
+use std::io;
 
 #[derive(Serialize, Deserialize)]
 pub struct Todo {
-    pub text: String
+    pub text: String,
 }
 
-pub fn read_json_file(filename:String) {
+pub fn read_json_file(filename: String) -> Vec<Todo> {
     let path = Path::new(&filename);
-    let mut fdata = String::new();
-    let mut rfile = File::open(path).expect("File not found");
-    rfile.read_to_string(&mut fdata).expect("File can't be read");
-    let todos: Vec<Todo> = serde_json::from_str(&fdata).unwrap();
-    for todo in todos {
-        println!("TODO: {}", todo.text)
+    
+    if !path.exists() {
+        println!("File not found, starting with an empty list.");
+        return vec![];
     }
+
+    let mut fdata = String::new();
+    let mut rfile = File::open(path).expect("Unable to open file");
+    rfile.read_to_string(&mut fdata).expect("Unable to read file");
+    
+    serde_json::from_str(&fdata).unwrap_or_else(|_| vec![])
 }
 
-pub fn write_json_file(filename: String, todos: &Vec<Todo>) {
+pub fn write_json_file(filename: String, new_todo: Todo) {
+    let mut todos = read_json_file(filename.clone());
+    todos.push(new_todo);
     let mut file = File::create(filename).expect("Unable to create file");
     let json = serde_json::to_string_pretty(&todos).expect("Unable to serialize data");
     file.write_all(json.as_bytes()).expect("Unable to write data to file");
+
+    println!("Added successfully!");
+}
+
+pub fn get_user_input() -> Todo {
+    println!("Enter text:");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    Todo {
+        text: input.trim().to_string(),
+    }
 }
